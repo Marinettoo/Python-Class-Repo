4728472ab71cf84e50035095b0938133d172c085
marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-07 18:28:58 +0200

    Ejercicio 1 y Ejercicio 2

--- Ejercicios Cadenas/01.EJERCICIO1.py
+++ Ejercicios Cadenas/01.EJERCICIO1.py
@@ -1,3 +1,6 @@
+'''Escribir un programa que pregunte el nombre del usuario en la consola y un número
+entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces
+como el número introducido.'''
 nombre = input('¿Como te llamas?')
 numero = int(input('¿Cuantas veces quieres repetir el nombre?'))
 print((nombre + '\n') * numero)
---
+++ Ejercicios Cadenas/02.EJERCICIO2.py
@@ -0,0 +1,4 @@
+nombre_completo = input('¿Cuál es tu nombre completo? ')
+print (nombre_completo.lower())
+print(nombre_completo.upper())
+print(nombre_completo.title())
