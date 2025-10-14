60c6811167db8a200e229e0494247be06259ac90
Marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-14 13:55:31 +0200

    Ejercicio 6

---
+++ Ejercicios Condicionales/06.EJERCICIO6.py
@@ -0,0 +1,16 @@
+'''Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y
+el nombre. El grupo A está formado por las mujeres con un nombre anterior a la M y
+los hombres con un nombre posterior a la N y el grupo B por el resto. Escribir un
+programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo
+que le corresponde.'''
+
+nombre = input('¿Como te llamas? ')
+sexo = input('¿Cual es tu sexo? (M ó H) ')
+
+nombre = nombre.capitalize()
+sexo = sexo.capitalize()
+
+if (sexo == 'M' and nombre[0] < 'M') or (sexo == 'H' and nombre[0] > 'N'):
+    print ('Eres del grupo 1')
+else:
+    print ('Eres del grupo 2')
