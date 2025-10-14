26a897fe08479a18051ae5ac162f2853b94c60de
Marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-14 13:32:14 +0200

    Ejercicio 4

---
+++ Ejercicios Condicionales/04.EJERCICIO4.py
@@ -0,0 +1,8 @@
+'''Escribir un programa que pida al usuario un número entero y muestre por pantalla si
+es par o impar.'''
+num = float(input(' Introduce un numero rey ' ))
+
+if num % 2 == 0:
+    print(' Es un número par ')
+else:
+    print(' Es un numero impar ')
