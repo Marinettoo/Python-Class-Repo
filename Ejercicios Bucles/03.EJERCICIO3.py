a5a7ed546f7d23921d8c33486cdd09c97aec4ee0
Marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-21 13:40:46 +0200

    Ejercicio 3

---
+++ Ejercicios Bucles/03.EJERCICIO3.py
@@ -0,0 +1,10 @@
+'''Escribir un programa que pida al usuario un número entero positivo y muestre por
+pantalla todos los números impares desde 1 hasta ese número separados por
+comas.'''
+
+numero = int(input('Introduce un numero entero positivo: '))
+n = -1
+
+while n < (numero):
+    n = n + 2
+    print (n, end=',')
