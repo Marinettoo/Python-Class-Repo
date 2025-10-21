8565b0373f67dea0a3c61d998987c6eef4fce465
Marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-21 14:12:01 +0200

    Ejercicio 7

---
+++ Ejercicios Bucles/07.EJERCICIO7.py
@@ -0,0 +1,10 @@
+'''Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.'''
+numero = int(input('introduce un numero para multiplicar'))
+n=0
+while n < 10:
+    n = n+1
+
+    multiplicación = n*numero
+    print (multiplicación)
+
+    
