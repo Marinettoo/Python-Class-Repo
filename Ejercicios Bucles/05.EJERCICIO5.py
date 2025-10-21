ce141319a6423ef61db1e5f799f9ae99dba1975b
Marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-21 14:00:35 +0200

    Corrección enumeracion

--- Ejercicios Bucles/05.EJERCICIO5.py
+++ Ejercicios Bucles/05.EJERCICIO5.py
@@ -1,13 +1,13 @@
-'''Escribir un programa que pida al usuario un número entero y muestre por pantalla
-un triángulo rectángulo como el de más abajo, de altura el número introducido.
-*
-**
-***
-****
-*****
-'''
+'''Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual
+y el número de años, y muestre por pantalla el capital obtenido en la inversión cada
+año que dura la inversión.'''
 
-altura = int(input('Inserta la altura del triángulo '))
+''''''
 
-for i in range (1, altura+1):
-    print ('*' * i)
+cantidad = float(input('¿Cuanto vas a invertir? '))
+interes = float(input('¿Cual es el interes anual? '))
+años = float(input('¿Cuantos años fura tu inversión?'))
+
+final= cantidad * (1+interes/100)**años
+
+print (final)
